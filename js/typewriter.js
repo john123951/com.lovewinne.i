664651128dd6f0f0fe52c0c5f21ec645d4ff5865
sweet <sweet@PC-Debian.lan>
2016-07-31 10:50:59 +0800

    init

---
+++ js/typewriter.js
@@ -0,0 +1,31 @@
+﻿(function (a) {
+    a.fn.typewriter = function (timeout, endCallBack, startCallBack) {
+        var arr = this;
+
+        arr.each(function (index, element) {
+            var d = a(element), c = d.html(), b = 0;
+            d.html("");
+
+            setTimeout(eventHandler, timeout);
+
+            function eventHandler() {
+                if (typeof (startCallBack) == "function") { startCallBack(); }
+
+                var e = setInterval(function () {
+                    var f = c.substr(b, 1);
+                    if (f == "<") {
+                        b = c.indexOf(">", b) + 1;
+                    } else {
+                        b++;
+                    }
+                    d.html(c.substring(0, b) + (b & 1 ? "_" : ""));
+                    if (b >= c.length) {
+                        clearInterval(e);
+                        if (typeof (endCallBack) == "function") { endCallBack(); }
+                    }
+                }, 75);
+            }
+        });
+        return this;
+    }
+})(jQuery);
---
+++ js/util.js
@@ -0,0 +1,47 @@
+﻿function calcTimeSpan(startTime, endTime) {
+    var timeSpan = endTime.getTime() - startTime.getTime();  //时间差的毫秒数
+
+    //计算出相差天数
+    var days = Math.floor(timeSpan / (24 * 3600 * 1000));
+
+    //计算出小时数
+
+    var leave1 = timeSpan % (24 * 3600 * 1000);    //计算天数后剩余的毫秒数
+    var hours = Math.floor(leave1 / (3600 * 1000));
+    //计算相差分钟数
+    var leave2 = leave1 % (3600 * 1000);       //计算小时数后剩余的毫秒数
+    var minutes = Math.floor(leave2 / (60 * 1000));
+
+    //计算相差秒数
+    var leave3 = leave2 % (60 * 1000);     //计算分钟数后剩余的毫秒数
+    var seconds = Math.round(leave3 / 1000);
+
+    return { days: days, hours: hours, minutes: minutes, seconds: seconds };
+}
+
+function ContentPicker(contentList) {
+    var me = this;
+    var _contentList = contentList;
+    var _position = 0;
+
+    var len = _contentList.length;
+
+    var swap = function (a, b) {
+        var temp = _contentList[a];
+        _contentList[a] = _contentList[b];
+        _contentList[b] = temp;
+    }
+
+    for (var i = 0; i < len / 2; i++) {
+        var index = Math.floor(Math.random() * len);
+        swap(index, i);
+    }
+
+    me.getNext = function () {
+        var result = _contentList[_position++];
+
+        if (_position >= _contentList.length) _position = 0;
+
+        return result;
+    }
+}
